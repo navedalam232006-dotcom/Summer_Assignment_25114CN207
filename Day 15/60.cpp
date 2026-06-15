f6dcc20ecec77b1b8037730f03131be5038dd967
navedalam232006-dotcom <navedalam232006@gmail.com>
2026-06-15 10:56:09 +0530

    Day 15

---
+++ Day 15/60.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int j = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+
+    cout << "Array after moving zeroes: ";
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
---
+++ Day 16/61.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n-1];
+    int sum = 0;
+
+    for(int i = 0; i < n-1; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    int expected = n * (n + 1) / 2;
+
+    cout << "Missing Number = " << expected - sum;
+
+    return 0;
+}
