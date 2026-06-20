121e32ea604895826f4fa0fdafce3387af22e1ce
navedalam232006-dotcom <navedalam232006@gmail.com>
2026-06-20 11:08:56 +0530

    Day 80

---
+++ Day 20/78.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter order of square matrix: ";
+    cin >> n;
+
+    int A[n][n];
+    bool symmetric = true;
+
+    cout << "Enter matrix elements:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(A[i][j] != A[j][i])
+            {
+                symmetric = false;
+                break;
+            }
+        }
+    }
+
+    if(symmetric)
+        cout << "Symmetric Matrix";
+    else
+        cout << "Not Symmetric Matrix";
+
+    return 0;
+}
---
+++ Day 20/80.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r, c;
+
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    int A[r][c];
+
+    cout << "Enter matrix elements:\n";
+
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    for(int j = 0; j < c; j++)
+    {
+        int sum = 0;
+
+        for(int i = 0; i < r; i++)
+        {
+            sum += A[i][j];
+        }
+
+        cout << "Column " << j + 1 << " Sum = " << sum << endl;
+    }
+
+    return 0;
+}
